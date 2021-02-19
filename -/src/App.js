bff563b8f973066970120fe16c15d2c9998944e5
Abir <abir.zar@gmail.com>
2021-02-19 21:07:09 +0100

    first commit

---
+++ -/src/App.js
@@ -0,0 +1,19 @@
+import React, { component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+class App extends component {
+
+  render() {
+
+    return (
+      <div className= "App">
+        <h1> Hello World </h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
